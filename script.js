a7acd6a409896d894cdf1fb4a60e0e87b01872e4
Abdur Rahim Bhuiyan <zamshedbhai@yahoo.com>
2024-04-22 22:25:29 +0000

    organise codes

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-(function() {
+(function () {
     let screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
     let clear = document.querySelector('.btn-clear');
     let equal = document.querySelector('.btn-equal');
 
-    buttons.forEach(function(button) {
+    buttons.forEach(function (button) {
         button.addEventListener('click', function (e) {
-            if(e.target.classList.contains('btn-equal')) {
+            if (e.target.classList.contains('btn-equal')) {
                 console.log("Just execute Equal Button");
-;            } else {
+                ;
+            } else {
                 let value = e.target.dataset.num;
                 screen.value += value;
             }
         })
     });
 
-    equal.addEventListener('click', function(e){
+    equal.addEventListener('click', function (e) {
         if (screen.value === '') {
             screen.value = "";
-        }else {
+        } else {
             let answer = eval(screen.value);
             screen.value = answer;
+
         }
     });
-    clear.addEventListener('click', function(e){
+    clear.addEventListener('click', function (e) {
         screen.value = "";
     });
-}) ();
+})();
